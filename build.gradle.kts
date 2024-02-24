200f520c1571580de14046db4f90979c6c2ae716
Thuseeman <thushfit@gmail.com>
2024-02-24 12:23:44 +0000

    Update ksp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
-//    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
 
